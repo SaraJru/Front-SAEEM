f7b0c836a15bfa177617ab2cd5f0be8933664b8f
SaraJru <sarajr_2.0@hotmail.com>
2023-08-24 03:05:36 -0600

    Login view done

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <form action="">
+            <div class="user-logo">
+                <i class='bx bxs-user-badge'></i>
+            </div>
+            <div class="input">
+                <input type="text" placeholder="Username" required>
+            </div>
+            <div class="input">
+                <input type="text" placeholder="Password" required>
+            </div>
+            <br>
+            <div>
+                <button type="submit" class="btn-login">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
