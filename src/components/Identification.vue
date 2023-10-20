e0dc90e966922214bb77447e590eb72e1c6dbcdd
Ricardo <luisrijhn09@gmail.com>
2023-10-19 21:38:49 -0600

    update: Métodos de creación y actualización

--- src/components/Identification.vue
+++ src/components/Identification.vue
@@ -1,6 +1,7 @@
 <script>
 
 import axios from 'axios'
+import router from '../router';
 
 export default {
     name: 'Identification',
@@ -12,11 +13,17 @@ export default {
             insert_date: "",
             update_date: "",
             listIdentifications: [],
+            editIdentificationName: "",
+            editIdentificationIndex: -1
         }
     },
     methods: {
         limpiar(){
             this.identification_name = "";
+            router.push('/identificationType')
+        },
+        limpiarUpdate(){
+            location.reload();
         },
         createIdentificationType(){
             axios.post('catalog/identificationType/', {
@@ -31,6 +38,19 @@ export default {
             this.listar();
             this.limpiar();
         },
+        updateVillage(){
+            axios.patch(`catalog/identificationType/${this.id}/`, {
+                identification_name: this.editIdentificationName
+            })
+            .then(response => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            this.listar();
+            this.limpiarUpdate();
+        },
         listar(){
             axios.get('catalog/identificationType/')
                 .then(response => {
@@ -42,6 +62,11 @@ export default {
                 })
                 .finally(function(){
                 })
+        },
+        openEditModal(i){
+            this.editIdentificationIndex = i;
+            this.editIdentificationName = this.listIdentifications[i].identification_name;
+            this.id = this.listIdentifications[i].id;
         }
     },
     mounted() {
@@ -55,13 +80,31 @@ export default {
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
-                This week
-            </button>
+                <div class="btn-group me-2">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalCreationID">
+                    Registrar nuevo tipo de identificación
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="ModalCreationID" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Registro de Tipos de Identificación</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="identificationName" class="form-label">Tipo de Identificación</label>
+                                    <input type="text" class="form-control" id="identificationName" placeholder="Tipo de identificación" v-model="identification_name">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="limpiar">Cerrar</button>
+                                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" @click="createIdentificationType">Crear</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -74,26 +117,44 @@ export default {
                 <th scope="col">Tipo de identificación</th>
                 <th scope="col">Fecha de creación</th>
                 <th scope="col">Fecha de actualización</th>
+                <th scope="col">Acciones</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="li in listIdentifications" v-bind:key="li.id">
+                <tr v-for="(li, i) in listIdentifications" v-bind:key="li.id">
                 <td>{{li.id}}</td>
                 <td>{{li.identification_name}}</td>
                 <td>{{li.insert_date}}</td>
                 <td>{{li.update_date}}</td>
+                <td>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModelUpdateID" @click="openEditModal(i)">
+                        Editar
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="ModelUpdateID" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Actualización de Localidades</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="actualIDname" class="form-label">Nombre de la Localidad</label>
+                                    <input type="text" class="form-control" id="actualIDname" placeholder="" v-model="editIdentificationName">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="updateVillage">Guardar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
                 </tr>
             </tbody>
             </table>
-        </div>
-        <div>
-            <div class="mb-3">
-                <label for="identificationName" class="form-label">Email address</label>
-                <input type="text" class="form-control" id="identificationName" placeholder="Tipo de identificación" v-model="identification_name">
-            </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3" @click="createIdentificationType">Crear</button>
-            </div>
         </div>
     </main>
 </template>
--- src/components/WattmeterBrand.vue
+++ src/components/WattmeterBrand.vue
@@ -12,11 +12,17 @@ export default {
             insert_date: "",
             update_date: "",
             listwattmeterBrand: [],
+            editBrandName: "",
+            editBrandNameIndex: -1
         }
     },
     methods: {
         limpiar(){
             this.brand_name = "";
+            router.push('/wattmeterBrand')
+        },
+        limpiarUpdate(){
+            location.reload();
         },
         createwattmeterBrand(){
             axios.post('catalog/wattmeterBrand/', {
@@ -31,6 +37,19 @@ export default {
             this.listar();
             this.limpiar();
         },
+        updateWattmeterBrand(){
+            axios.patch(`catalog/wattmeterBrand/${this.id}/`, {
+                brand_name: this.editBrandName
+            })
+            .then(response => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            this.listar();
+            this.limpiarUpdate();
+        },
         listar(){
             axios.get('catalog/wattmeterBrand/')
                 .then(response => {
@@ -42,6 +61,11 @@ export default {
                 })
                 .finally(function(){
                 })
+        },
+        openEditModal(i){
+            this.editBrandNameIndex = i;
+            this.editBrandName = this.listwattmeterBrand[i].brand_name;
+            this.id = this.listwattmeterBrand[i].id;
         }
     },
     mounted() {
@@ -55,13 +79,31 @@ export default {
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
-            <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
-            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center gap-1">
-                This week
-            </button>
+                <div class="btn-group me-2">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalCreationBrand">
+                    Registrar nueva marca de contador
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="ModalCreationBrand" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Registro de Tipos de Identificación</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="brandName" class="form-label">Nombre de marca</label>
+                                    <input type="text" class="form-control" id="brandName" placeholder="Nombre de la marca" v-model="brand_name">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="limpiar">Cerrar</button>
+                                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" @click="createwattmeterBrand">Crear</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -74,26 +116,44 @@ export default {
                 <th scope="col">Nombre de la marca</th>
                 <th scope="col">Fecha de creación</th>
                 <th scope="col">Fecha de actualización</th>
+                <th scope="col">Acciones</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="lw in listwattmeterBrand" v-bind:key="lw.id">
+                <tr v-for="(lw, i) in listwattmeterBrand" v-bind:key="lw.id">
                 <td>{{lw.id}}</td>
                 <td>{{lw.brand_name}}</td>
                 <td>{{lw.insert_date}}</td>
                 <td>{{lw.update_date}}</td>
+                <td>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModelUpdateBrand" @click="openEditModal(i)">
+                        Editar
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="ModelUpdateBrand" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Actualización de Marcas</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <label for="actualBrandName" class="form-label">Nombre de la marca</label>
+                                    <input type="text" class="form-control" id="actualBrandName" placeholder="" v-model="editBrandName">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="updateWattmeterBrand">Guardar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
                 </tr>
             </tbody>
             </table>
-        </div>
-        <div>
-            <div class="mb-3">
-                <label for="brandName" class="form-label">Email address</label>
-                <input type="text" class="form-control" id="brandName" placeholder="Nombre de la marca" v-model="brand_name">
-            </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3" @click="createwattmeterBrand">Crear</button>
-            </div>
         </div>
     </main>
 </template>
